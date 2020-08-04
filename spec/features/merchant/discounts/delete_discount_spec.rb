57ee9633a4e1a3268657f4b56a977b9e194f3875
Norma Lopez <norma_l304@yahoo.com>
2020-08-04 15:49:05 -0600

    Remove stub for current_user and manually login user

--- spec/features/merchant/discounts/delete_discount_spec.rb
+++ spec/features/merchant/discounts/delete_discount_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe 'Destroy a Discount' do
       @m_user = @merchant_1.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword', role: 1)
       @discount1 = @merchant_1.discounts.create!(name: "15% off 10 or more items", discount_percentage: 15, minimum_quantity: 10)
       @discount2 = @merchant_1.discounts.create!(name: "50% off 100 or more items", discount_percentage: 50, minimum_quantity: 100)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+      visit "/login"
+      fill_in :email, with: @m_user.email
+      fill_in :password, with: @m_user.password
+      click_button "Log In"
     end
 
     it 'I can delete a discount' do
@@ -22,6 +25,7 @@ RSpec.describe 'Destroy a Discount' do
 
       expect(current_path).to eq("/merchant/discounts")
       expect(page).to have_content(@discount1.name)
+      expect(page).to_not have_content(@discount2.name)
     end
   end
 end
