fec715ff429e7e7298f95f1ea5cfc5ae2d703450
Norma Lopez <norma_l304@yahoo.com>
2020-08-04 23:36:25 -0600

    Test order can get created with discount applied

--- spec/features/user/orders/create_spec.rb
+++ spec/features/user/orders/create_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Create Order' do
     before :each do
       @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
       @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @discount1 = @megan.discounts.create!(name: "15% off 5 or more items", discount_percentage: 15, minimum_quantity: 1)
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
@@ -37,14 +38,23 @@ RSpec.describe 'Create Order' do
     end
 
     it 'reflects total with applied discounts' do
-      visit '/profile/orders'
+      visit item_path(@ogre)
+      click_button 'Add to Cart'
+      visit item_path(@hippo)
+      click_button 'Add to Cart'
 
-      within "#order-#{@order_1.id}" do
-        expect(page).to have_content("Total: $34.43")
-      end
+      visit '/cart'
 
-      within "#order-#{@order_2.id}" do
-        expect(page).to have_content("Total: $115.38")
+      click_button 'Check Out'
+
+      order = Order.last
+
+      expect(current_path).to eq('/profile/orders')
+      expect(page).to have_content('Order created successfully!')
+      expect(page).to have_link('Cart: 0')
+
+      within "#order-#{order.id}" do
+        expect(page).to have_content("Total: $67")
       end
     end
   end
